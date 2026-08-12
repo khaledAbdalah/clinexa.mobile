ccda92e4ad10c9d595414164e231f89cc7610d3f
Khaled Abdallah <khaledabdalah488@gmail.com>
2026-08-13 00:47:15 +0300

    feat: simplify layout and index components, remove reset project script

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,40 +1,5 @@
-import { PortalHost } from '@rn-primitives/portal';
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import { useColorScheme } from 'react-native';
-import Toast from 'react-native-toast-message';
+import { Stack } from "expo-router";
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
-import { ErrorBoundary } from '@/components/other/error-boundary';
-import { toastConfig } from '@/config/toast';
-import { useLoadFonts } from '@/hooks/use-load-fonts';
-
-SplashScreen.preventAutoHideAsync();
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-  const fontsLoaded = useLoadFonts();
-
-  useEffect(() => {
-    if (fontsLoaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
-
-  if (!fontsLoaded) {
-    return null;
-  }
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <ErrorBoundary>
-        <AppTabs />
-      </ErrorBoundary>
-      <PortalHost />
-      <Toast config={toastConfig} />
-    </ThemeProvider>
-  );
+export default function RootLayout() {
+  return <Stack />;
 }
